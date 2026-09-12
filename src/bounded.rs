bffeb2b6771ad1ed5de0ef610d1e90fb16293814
cazeth <50043106+cazeth@users.noreply.github.com>
2026-09-12 08:07:15 +0200

    refactor(bounded): make filter more concise

--- src/bounded.rs
+++ src/bounded.rs
@@ -271,8 +271,7 @@ pub trait Bounded: BoundSeal {
         let y: Option<i32> = self
             .y_max_boundary()
             .checked_sub_unsigned(distance[1])
-            .filter(|y| *y > i32::MIN)
-            .filter(|y| *y >= self.y_min_boundary());
+            .filter(|y| *y > i32::MIN && *y >= self.y_min_boundary());
 
         match (x, y) {
             (Some(x), Some(y)) => Ok(Coordinate { x, y }),
